23b0bdc28784210aef3cf1b4d256895af0c98b94
AsminSayim <145451871+AsminSayim@users.noreply.github.com>
2023-12-12 13:27:35 +0300

    Add files via upload

---
+++ sayı-tahmin-oyun/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace sayı_tahmin_oyun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.btnTahminEt = new System.Windows.Forms.Button();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTahmin = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBasla.Location = new System.Drawing.Point(81, 23);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(150, 58);
+            this.btnBasla.TabIndex = 0;
+            this.btnBasla.Text = "Başla";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // btnTahminEt
+            // 
+            this.btnTahminEt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTahminEt.Location = new System.Drawing.Point(82, 296);
+            this.btnTahminEt.Name = "btnTahminEt";
+            this.btnTahminEt.Size = new System.Drawing.Size(150, 58);
+            this.btnTahminEt.TabIndex = 1;
+            this.btnTahminEt.Text = "Tahmin Et";
+            this.btnTahminEt.UseVisualStyleBackColor = true;
+            this.btnTahminEt.Click += new System.EventHandler(this.btnTahminEt_Click);
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSkor.Location = new System.Drawing.Point(119, 167);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(67, 25);
+            this.lblSkor.TabIndex = 2;
+            this.lblSkor.Text = "Skor:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(77, 254);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tahminin Nedir";
+            // 
+            // txtTahmin
+            // 
+            this.txtTahmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTahmin.Location = new System.Drawing.Point(82, 98);
+            this.txtTahmin.Name = "txtTahmin";
+            this.txtTahmin.Size = new System.Drawing.Size(149, 31);
+            this.txtTahmin.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(322, 374);
+            this.Controls.Add(this.txtTahmin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblSkor);
+            this.Controls.Add(this.btnTahminEt);
+            this.Controls.Add(this.btnBasla);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Tahmin Oyunu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Button btnTahminEt;
+        private System.Windows.Forms.Label lblSkor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTahmin;
+    }
+}
+
---
+++ sayı-tahmin-oyun/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayı_tahmin_oyun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double sayi;
+        int skor;
+
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            skor = 100;
+            lblSkor.Text = "Skor : " + skor.ToString();
+            btnTahminEt.Enabled = true;
+            Random rastgele = new Random();
+            sayi = rastgele.Next(101);
+            MessageBox.Show("Aklımdan Bir Sayı Tuttum.");
+        }
+
+        private void btnTahminEt_Click(object sender, EventArgs e)
+        {
+
+            int tahmin = Convert.ToInt32(txtTahmin.Text);
+            if (tahmin < sayi)
+            {
+                MessageBox.Show("Yukarı");
+                skor = skor - 10;
+            }
+            else if (tahmin > sayi)
+            {
+                MessageBox.Show("Aşağı");
+                skor = skor - 10;
+            }
+            else
+            {
+                MessageBox.Show("Tebrikler..:))");
+            }
+
+            if (skor == 0)
+            {
+                MessageBox.Show("GAME OVER!!!!!");
+                btnTahminEt.Enabled = false;
+            }
+            lblSkor.Text = "Skor : " + skor.ToString();
+            txtTahmin.Clear();
+        }
+    
+    }
+}
